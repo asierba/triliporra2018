b6679fc515e479a191c2d294589fcd931ffd296a
Asier <1537266+asierba@users.noreply.github.com>
2018-04-27 20:56:57 +0100

    use webpack to bundle js, css and icons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.get('/api/ping', (req, res) => res.send('pong'));
 app.listen(3000);
