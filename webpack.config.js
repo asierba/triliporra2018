2d3d68eba5b587b9a0493b0d0bdf675bd8e91b37
Asier <asierba@gmail.com>
2018-04-29 19:26:40 +0100

    index file gets copied from src to dist at build time

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -22,5 +23,12 @@ module.exports = {
                 ]
             }
       ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+        {
+          from: 'src/index.html',
+          to: 'index.html'
+        }])
+  ]
 };
