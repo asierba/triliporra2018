252dda21b19358f9a7c73e75b203f360ba626259
Asier <asierba@gmail.com>
2018-05-01 09:00:43 +0100

    Introduce routes for home page and matches page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,34 @@
-import bar from './bar';
 import './style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as BrowserRouter, Router, Route, Link } from "react-router-dom";
 
-const text = bar();
+function HomePage(props) {
+  return <h1>This is the home page</h1>;
+}
+function MatchesPage(props) {
+  return <h1>List of matches here</h1>;
+}
+
+const App = () => (
+  <div>
+    <nav>
+      <ul className="nav nav-tabs">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="matches">Matches</Link></li>
+      </ul>
+    </nav>
+    <div>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/matches" component={MatchesPage}/>
+    </div>
+  </div>
+);
 
 ReactDOM.render(
-    <h1>{text}</h1>,
+    (<BrowserRouter>
+    <App/>
+    </BrowserRouter>),
     document.getElementById('root')
   );
+
