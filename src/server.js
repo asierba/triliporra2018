a964aa8c202c97b5ed1615fe874e349a4295c3ba
Asier <asierba@gmail.com>
2018-05-01 17:32:03 +0100

    react urls work when reloading the browser or directly navigating to them

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.use(express.static('dist'));
 
 app.get('/api/ping', (req, res) => res.send('pong'));
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
+
 app.listen(3000);
