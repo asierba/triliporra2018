b6f0aae5158070a494ee734b22197d0370705823
Asier <1537266+asierba@users.noreply.github.com>
2018-05-10 19:11:10 +0100

    Matches page shows a result to the user which can be edited

--- src/components/match/matchListPage.js
+++ src/components/match/matchListPage.js
@@ -1,31 +1,70 @@
 import React from 'react';
 
+function getMatches() {
+  const url = "/api/match";
+  const xmlHttp = new XMLHttpRequest();
+  const async = false;
+  xmlHttp.open("GET", url, async);
+  xmlHttp.send();
 
-const url = "/api/match";
-const xmlhttp = new XMLHttpRequest();
-const async = false;
-xmlhttp.open("GET", url, async);
-xmlhttp.send();
+  const response = JSON.parse(xmlHttp.responseText);
+  return response.entities;
+}
 
-const response=JSON.parse(xmlhttp.responseText);
+export default class MatchesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      matches: getMatches()
+    };
+  }
 
-export default function MatchesPage(props) {
-  return {
-    state: {
-      matches: response.entities
-    },
-    render: function() {
-      const li = (match) => <li>{match.home} vs {match.away} - {match.date}</li>;
+  changeScore(matchId, type, event) {
+    const value = event.target.value;
 
-      return (
-        <div>
-          <h1>Upcoming matches</h1>
-          <ul>
-            {this.state.matches.map(li)}
-          </ul>
-        </div>
-      );
-    },
+    const matches = this.state.matches.map(match => {
+      if (match.id === matchId) {
+        match.score[type] = value;
+      }
+      return match;
+    })
 
+
+    this.setState({matches});
+  }
+
+  onSave(match) {
+    const url = `/api/match/${match.id}`;
+    const xmlHttp = new XMLHttpRequest();
+    const async = true;
+    xmlHttp.open("PATCH", url, async);
+    xmlHttp.setRequestHeader("Content-type", "application/json");
+    let matchDATA = {
+      score : {
+        home: match.score.home,
+        away: match.score.away
+      }
+    };
+    xmlHttp.send(JSON.stringify(matchDATA));
+  }
+
+  render() {
+    const li = (match) =>
+      (<li>
+        {match.date} | <span class="home">{match.home}</span>
+        <input type="text" value={match.score.home} onChange={this.changeScore.bind(this, match.id, 'home')} /> -
+        <input type="text" value={match.score.away} onChange={this.changeScore.bind(this, match.id, 'away')}  />
+        <span class="away">{match.away}</span>
+        <input type="submit" value="Save" className="btn btn-default" onClick={this.onSave.bind(this, match)} />
+      </li>);
+
+    return (
+      <div id="matches">
+        <h1>Upcoming matches</h1>
+        <ul>
+          {this.state.matches.map(li)}
+        </ul>
+      </div>
+    );
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.use(express.static('dist'));
 
+const matches = require('./matches.json');
+const matchesWithResults = matches.map(x => Object.assign(x, { score: { home: 0, away: 0}}));
+
 app.get('/api/match', (req, res) => {
-  const matches = require('./matches.json');
   const response = { entities: matches };
   res.send(response);
+});
+
+app.patch('/api/match/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const body = req.body;
+
+  const match = matchesWithResults.find(x => x.id === id);
+
+  if (!match) {
+    res.status(404);
+    res.send("match not found");
+    return;
+  }
+
+  match.score = body.score;
+
+  res.send(match)
 });
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../dist/index.html')));
