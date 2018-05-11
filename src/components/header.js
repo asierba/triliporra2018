c09183215e777bd0842440bc7978da5ac1872f9b
Asier <1537266+asierba@users.noreply.github.com>
2018-05-11 17:52:16 +0100

    incorporate design for header

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,55 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 export default function Header(props) {
-  return (
-    <nav>
-      <ul className="nav nav-tabs">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="matches">Matches</Link></li>
-      </ul>
-    </nav>);
-}
+    return (
+
+      <header id="sportsmagazine-header" className="sportsmagazine-header-one">
+        <div className="sportsmagazine-topstrip">
+          <div className="container">
+            <div className="row">
+              <aside className="col-md-4">
+                <ul className="sportsmagazine-social-network">
+                  <li><a href="#" className="sportsmagazine-colorhover fa fa-facebook-official" /></li>
+                  <li><a href="#" className="sportsmagazine-colorhover fa fa-twitter-square" /></li>
+                  <li><a href="#" className="sportsmagazine-colorhover fa fa-linkedin-square" /></li>
+                  <li><a href="#" className="sportsmagazine-colorhover fa fa-google-plus-square" /></li>
+                </ul>
+              </aside>
+              <aside className="col-md-8">
+                <ul className="sportsmagazine-user-section">
+                  <li><i className="fa fa-user" /> <a href="#">Login</a></li>
+                  <li><i className="fa fa-sign-in" /> <a href="#">Sign Up</a></li>
+                </ul>
+              </aside>
+            </div>
+          </div>
+        </div>
+        <div className="sportsmagazine-main-header">
+          <div className="container">
+            <div className="row">
+              <aside className="col-md-2">
+                <Link to="/" className="sportsmagazine-logo"><img src="images/logo-3.png" alt="logo" /></Link>
+              </aside>
+              <div className="col-md-10">
+                <div className="sportsmagazine-right-section">
+                  <nav className="navbar navbar-default">
+                    <div className="collapse navbar-collapse" id="navbar-collapse-1">
+                      <ul className="nav navbar-nav">
+                        <li>
+                          <Link to="/">Home</Link>
+                        </li>
+                        <li className="sportsmagazine-megamenu-li">
+                          <Link to="matches">Matches</Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+};
+
