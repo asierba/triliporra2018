f0f90b55bb153e59c118fae3a01c2ffaf63e6f45
Asier Barrenetxea <asierba@gmail.com>
2018-05-22 12:06:36 +0100

    in production env variables are not retrived from .env file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const dotenv = require('dotenv');
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const app = express();
 app.use(bodyParser.json());
