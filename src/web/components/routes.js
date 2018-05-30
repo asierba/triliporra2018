ca7ac7f3b36ba954c459e61550496844705b8a6a
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2018-05-30 22:15:26 +0100

    integrate auth0 for login

--- src/web/components/routes.js
+++ src/web/components/routes.js
@@ -3,6 +3,8 @@ import { Route } from "react-router-dom";
 import HomePage from './home/homePage';
 import MatchesPage from './match/matchesPage';
 import InsertMatchesPage from './admin/insertMatchesPage';
+import CallbackPage from './login/callbackPage';
+
 
 export default function Routes(props) {
   return (
@@ -10,5 +12,6 @@ export default function Routes(props) {
       <Route exact path="/" component={HomePage} />
       <Route path="/matches" component={MatchesPage} />
       <Route path="/admin" component={InsertMatchesPage} />
+      <Route path="/callback" component={CallbackPage} />
     </div>);
 }
