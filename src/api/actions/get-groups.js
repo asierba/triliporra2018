61f14506f7b688c19bfdbf149f115a94bb17bd14
Asier <1537266+asierba@users.noreply.github.com>
2018-06-29 10:12:16 +0200

    simplify get-groups functions + add rambda.js

--- src/api/actions/get-groups.js
+++ src/api/actions/get-groups.js
@@ -1,44 +1,65 @@
 const repository = require('../repository');
+const R = require('ramda');
 
-const isHomeWin = match => teamName => match.home === teamName && match.score.home > match.score.away;
-const isAwayWin = match => teamName => match.away === teamName && match.score.home < match.score.away;
-const isAwayLose = match => teamName => match.away === teamName && match.score.home > match.score.away;
-const isHomeLose = match => teamName => match.home === teamName && match.score.home < match.score.away;
-const isHomeDraw = match => teamName => match.home === teamName && match.score.home === match.score.away
-const isAwayDraw = match => teamName => match.away === teamName && match.score.home === match.score.away
+function isHomeWin(match) {
+  return match.score.home > match.score.away;
+}
+function isAwayWin(match) {
+  return match.score.home < match.score.away;
+}
+function isDraw(match) {
+  return match.score.home === match.score.away;
+}
 
-const isWinForTeam = teamName => match => isHomeWin(match)(teamName) || isAwayWin(match)(teamName);
-const isLoseForTeam = teamName => match => isHomeLose(match)(teamName) || isAwayLose(match)(teamName);
-const isADrawFroTeam = teamName => match => isHomeDraw(match)(teamName) || isAwayDraw(match)(teamName);
+function teamPlaysHome(match, teamName) {
+  return match.home === teamName;
+}
 
-const convertToTeamDetails = matches => teamName => {
-  const isAWin = isWinForTeam(teamName);
-  const isALose = isLoseForTeam(teamName);
-  const isADraw = isADrawFroTeam(teamName);
+function teamPlaysAway(match, teamName) {
+  return match.away === teamName;
+}
 
-  const teamMatches = matches.filter(match => match.home === teamName || match.away === teamName);
-  const matchesWithScore = teamMatches.filter(match => match.score);
+function isWinForTeam(teamName, match) {
+  return teamPlaysHome(match, teamName) && isHomeWin(match)
+    || teamPlaysAway(match, teamName) && isAwayWin(match);
+}
 
+function isLoseForTeam(teamName, match) {
+  return teamPlaysHome(match, teamName) && isAwayWin(match)
+    || teamPlaysAway(match, teamName) && isHomeWin(match);
+}
+
+function sumGoalsScoredForTeam(teamName, result, match) {
+  if (match.home === teamName)
+    return result + match.score.home;
+  if (match.away === teamName)
+    return result + match.score.away;
+}
+
+function sumGoalsAgainstForTeam(teamName, result, match) {
+  if(match.home === teamName)
+    return result + match.score.away;
+  if(match.away === teamName)
+    return result + match.score.home;
+}
+
+function convertToTeamDetails(matches, teamName) {
+  const isAWin = R.curry(isWinForTeam)(teamName);
+  const isALose = R.curry(isLoseForTeam)(teamName);
+  const sumGoalsScored = R.curry(sumGoalsScoredForTeam)(teamName);
+  const sumGoalsAgainst = R.curry(sumGoalsAgainstForTeam)(teamName);
+
+  const matchesWithScore = matches
+    .filter(match => teamPlaysHome(match, teamName) || teamPlaysAway(match, teamName))
+    .filter(match => match.score);
+
+  const matchesPlayed = matchesWithScore.length;
   const wins = matchesWithScore.filter(isAWin).length;
   const loses = matchesWithScore.filter(isALose).length;
-  const draws = matchesWithScore.filter(isADraw).length;
-  const matchesPlayed = matchesWithScore.length;
+  const draws = matchesWithScore.filter(isDraw).length;
   const points = wins * 3 + draws;
-  const goalsScored = matchesWithScore
-    .reduce((result, match) => {
-      if(match.home === teamName)
-        return result + match.score.home;
-      if(match.away === teamName)
-        return result + match.score.away;
-    } ,0);
-
-  const goalsAgainst = matchesWithScore
-    .reduce((result, match) => {
-      if(match.home === teamName)
-        return result + match.score.away;
-      if(match.away === teamName)
-        return result + match.score.home;
-    } ,0);
+  const goalsScored = matchesWithScore.reduce(sumGoalsScored, 0);
+  const goalsAgainst = matchesWithScore.reduce(sumGoalsAgainst,0);
   const goalDifference = goalsScored - goalsAgainst;
   return {name: teamName, matchesPlayed, wins, loses, draws, points, goalsScored, goalsAgainst, goalDifference};
 }
@@ -49,7 +70,7 @@ const byGoalsScored = (match, otherMatch) => otherMatch.goalsScored - match.goal
 
 const getTeamsInGroupFromMatches = matches => groupName => {
   const groupMatches = matches.filter(match => match.stage === groupName);
-  const toTeamDetails = convertToTeamDetails(groupMatches);
+  const toTeamDetails = R.curry(convertToTeamDetails)(groupMatches);
 
   const teamNames = groupMatches.map(x => x.home).concat(groupMatches.map(x => x.away));
   const uniqueTeamNames = [...new Set(teamNames)];
