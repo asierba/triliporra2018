17760e1f7a4b031c6e7ec447f4890081a6d4b7c8
Asier <1537266+asierba@users.noreply.github.com>
2018-07-15 19:24:46 +0200

    Script to copy db puts backups in a folder

--- mongo-copy-prod.sh
+++ mongo-copy-prod.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 URI=$(head -n 1 .prod.env | sed -n 's/^CONNECTION_STRING=*//p')
+timestamp=$(date +%s)
 
 mongo triliporra --eval "db.dropDatabase()"
-mongodump --uri $URI
-mv dump/admin dump/triliporra
+mongodump --uri ${URI}
+mkdir -p backups/${timestamp}
+cp -r dump/ backups/${timestamp}
+mv dump/heroku_mzdd9pzd dump/triliporra
 mongorestore
 rm -rf dump/
