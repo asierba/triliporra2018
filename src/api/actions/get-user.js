6313a8de5d7c3cc5f31c31a34a35588a7f421c53
Asier <asierba@gmail.com>
2018-07-17 17:49:50 +0200

    Copy logic for missed/guessed to user endpoint

--- src/api/actions/get-user.js
+++ src/api/actions/get-user.js
@@ -12,18 +12,37 @@ function addPrediction(match, matchPredictions, userId) {
   return match;
 }
 
+function getScoreResult(score) {
+  if (score.home > score.away) {
+    return "home";
+  }
+  if (score.home < score.away) {
+    return "away";
+  }
+  return "draw";
+}
+
+function addResult(match) {
+  if (!match.score) return match;
+
+  const result = getScoreResult(match.score) === match.prediction ? 'guessed' : 'missed';
+  return Object.assign({}, match, { result });
+}
+
 function getUser(id) {
   return new Promise(resolve => {
     Promise.all([
       repository.getAll('match-prediction'),
       repository.getAll('match')
     ]).then(results => {
-      const [matchPredictions, matches] = results;
-      const matchesWithPredictions = matches.map(match => addPrediction(match, matchPredictions, id));
+      const [matchPredictions, allMatches] = results;
+      const matches = allMatches
+        .map(match => addPrediction(match, matchPredictions, id))
+        .map(match => addResult(match));
 
       resolve({
-        id: id,
-        matches: matchesWithPredictions
+        id,
+        matches
       });
     });
   });
