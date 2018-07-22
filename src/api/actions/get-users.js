52c967b89bc6a115edad40ab54075bacef0caeab
Asier <1537266+asierba@users.noreply.github.com>
2018-07-22 13:44:01 +0100

    /user endpoint returns the whole user with points

--- src/api/actions/get-users.js
+++ src/api/actions/get-users.js
@@ -1,10 +1,12 @@
 const R = require('ramda');
 const repository = require('../repository');
+const getUser = require('./get-user');
 
 async function getUsers() {
   const predictions = await repository.getAll('match-prediction');
   const userIds = R.uniq(predictions.map(x => x.userId));
-  return userIds.map(x => ({ id: x }));
+  const users = userIds.map(async id => await getUser(id));
+  return await Promise.all(users);
 }
 
 module.exports = getUsers;
