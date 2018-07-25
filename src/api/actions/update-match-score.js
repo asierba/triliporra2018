d5ce10b622ac29b53de1e3eb6c4fbed2b8880436
Asier <1537266+asierba@users.noreply.github.com>
2018-07-25 07:37:04 +0100

    Match patch can add penalties' score

--- src/api/actions/update-match-score.js
+++ src/api/actions/update-match-score.js
@@ -1,10 +1,22 @@
 const repository = require('../repository');
 
-function updateMatchScore(id, scoreUpdate) {
+function toScore(scoreUpdate) {
   const score = {
-    home : Number(scoreUpdate.home),
-    away : Number(scoreUpdate.away),
+    home: Number(scoreUpdate.home),
+    away: Number(scoreUpdate.away),
   };
+
+  if (!scoreUpdate.penalties) {
+    return score;
+  }
+
+  const penalties = toScore(scoreUpdate.penalties);
+
+  return Object.assign({}, score, {penalties: penalties});
+}
+
+function updateMatchScore(id, scoreUpdate) {
+  const score = toScore(scoreUpdate);
 
   return repository.update('match', {id}, {score});
 }
