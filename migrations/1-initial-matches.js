3bebe549237bbf1a367155e800c8f9a5d24d5c85
Asier <1537266+asierba@users.noreply.github.com>
2018-08-13 08:11:14 +0100

    Introduce Champions league 2017-18 group matches

--- migrations/1-initial-matches.js
+++ migrations/1-initial-matches.js
@@ -4,100 +4,107 @@ module.exports.id = "initial-matches";
 
 module.exports.up = function (done) {
   this.db.collection('match').insertMany([
-    {"id": 1,"date":"2018-06-14T15:00:00.000Z","home":"Russia","away":"Saudi Arabia","stage":"group stage"},
+    {"id" : 1, "date":"2018-09-12T19:00:00.000Z", "home":"Man. United" , "away":"Basel", "stage":"Group A"},
+    {"id" : 2, "date":"2018-09-12T19:00:00.000Z", "home":"Benfica", "away":"CSKA Moskva", "stage":"Group A"},
+    {"id" : 3, "date":"2018-09-12T19:00:00.000Z", "home":"Celtic", "away":"Paris", "stage":"Group B"},
+    {"id" : 4, "date":"2018-09-12T19:00:00.000Z", "home":"Bayern", "away":"Anderlecht", "stage":"Group B"},
+    {"id" : 5, "date":"2018-09-12T19:00:00.000Z", "home":"Roma", "away":"Atlético", "stage":"Group C"},
+    {"id" : 6, "date":"2018-09-12T19:00:00.000Z", "home":"Chelsea", "away":"Qarabağ", "stage":"Group C"},
+    {"id" : 7, "date":"2018-09-12T19:00:00.000Z", "home":"Olympiacos", "away":"Sporting CP", "stage":"Group D"},
+    {"id" : 8, "date":"2018-09-12T19:00:00.000Z", "home":"Barcelona", "away":"Juventus", "stage":"Group D"},
+    {"id" : 9, "date":"2018-09-13T19:00:00.000Z", "home":"Liverpool", "away":"Sevilla", "stage":"Group E"},
+    {"id" : 10, "date":"2018-09-13T19:00:00.000Z", "home":"Maribor", "away":"Spartak Moskva", "stage":"Group E"},
+    {"id" : 11, "date":"2018-09-13T19:00:00.000Z", "home":"Shakhtar Donetsk", "away":"Napoli", "stage":"Group F"},
+    {"id" : 12, "date":"2018-09-13T19:00:00.000Z", "home":"Feyenoord", "away":"Man. City", "stage":"Group F"},
+    {"id" : 13, "date":"2018-09-13T19:00:00.000Z", "home":"Porto", "away":"Beşiktaş", "stage":"Group G"},
+    {"id" : 14, "date":"2018-09-13T19:00:00.000Z", "home":"Leipzig", "away":"Monaco", "stage":"Group G"},
+    {"id" : 15, "date":"2018-09-13T19:00:00.000Z", "home":"Tottenham", "away":"Dortmund", "stage":"Group H"},
+    {"id" : 16, "date":"2018-09-13T19:00:00.000Z", "home":"Real Madrid", "away":"APOEL", "stage":"Group H"},
+    {"id" : 17, "date":"2018-09-26T19:00:00.000Z", "home":"Spartak Moskva", "away":"Liverpool", "stage":"Group E"},
+    {"id" : 18, "date":"2018-09-26T19:00:00.000Z", "home":"Sevilla", "away":"Maribor", "stage":"Group E"},
+    {"id" : 19, "date":"2018-09-26T19:00:00.000Z", "home":"Man. City", "away":"Shakhtar Donetsk", "stage":"Group F"},
+    {"id" : 20, "date":"2018-09-26T19:00:00.000Z", "home":"Napoli", "away":"Feyenoord", "stage":"Group F"},
+    {"id" : 21, "date":"2018-09-26T19:00:00.000Z", "home":"Monaco", "away":"Porto", "stage":"Group G"},
+    {"id" : 22, "date":"2018-09-26T19:00:00.000Z", "home":"Beşiktaş", "away":"Leipzig", "stage":"Group G"},
+    {"id" : 23, "date":"2018-09-26T19:00:00.000Z", "home":"APOEL", "away":"Tottenham", "stage":"Group H"},
+    {"id" : 24, "date":"2018-09-26T19:00:00.000Z", "home":"Dortmund", "away":"Real Madrid", "stage":"Group H"},
+    {"id" : 25, "date":"2018-09-27T19:00:00.000Z", "home":"Qarabağ", "away":"Roma", "stage":"Group C"},
+    {"id" : 26, "date":"2018-09-27T19:00:00.000Z", "home":"CSKA Moskva", "away":"Man. United", "stage":"Group A"},
+    {"id" : 27, "date":"2018-09-27T19:00:00.000Z", "home":"Basel", "away":"Benfica", "stage":"Group A"},
+    {"id" : 28, "date":"2018-09-27T19:00:00.000Z", "home":"Anderlecht", "away":"Celtic", "stage":"Group B"},
+    {"id" : 29, "date":"2018-09-27T19:00:00.000Z", "home":"Paris", "away":"Bayern", "stage":"Group B"},
+    {"id" : 30, "date":"2018-09-27T19:00:00.000Z", "home":"Atlético", "away":"Chelsea", "stage":"Group C"},
+    {"id" : 31, "date":"2018-09-27T19:00:00.000Z", "home":"Juventus", "away":"Olympiacos", "stage":"Group D"},
+    {"id" : 32, "date":"2018-09-27T19:00:00.000Z", "home":"Sporting CP", "away":"Barcelona", "stage":"Group D"},
+    {"id" : 33, "date":"2018-10-17T19:00:00.000Z", "home":"Maribor", "away":"Liverpool", "stage":"Group E"},
+    {"id" : 34, "date":"2018-10-17T19:00:00.000Z", "home":"Spartak Moskva", "away":"Sevilla", "stage":"Group E"},
+    {"id" : 35, "date":"2018-10-17T19:00:00.000Z", "home":"Feyenoord", "away":"Shakhtar Donetsk", "stage":"Group F"},
+    {"id" : 36, "date":"2018-10-17T19:00:00.000Z", "home":"Man. City", "away":"Napoli", "stage":"Group F"},
+    {"id" : 37, "date":"2018-10-17T19:00:00.000Z", "home":"Leipzig", "away":"Porto", "stage":"Group G"},
+    {"id" : 38, "date":"2018-10-17T19:00:00.000Z", "home":"Monaco", "away":"Beşiktaş", "stage":"Group G"},
+    {"id" : 39, "date":"2018-10-17T19:00:00.000Z", "home":"Real Madrid", "away":"Tottenham", "stage":"Group H"},
+    {"id" : 40, "date":"2018-10-17T19:00:00.000Z", "home":"APOEL", "away":"Dortmund", "stage":"Group H"},
+    {"id" : 41, "date":"2018-10-18T19:00:00.000Z", "home":"Qarabağ", "away":"Atlético", "stage":"Group C"},
+    {"id" : 42, "date":"2018-10-18T19:00:00.000Z", "home":"Benfica", "away":"Man. United", "stage":"Group A"},
+    {"id" : 43, "date":"2018-10-18T19:00:00.000Z", "home":"CSKA Moskva", "away":"Basel", "stage":"Group A"},
+    {"id" : 44, "date":"2018-10-18T19:00:00.000Z", "home":"Bayern", "away":"Celtic", "stage":"Group B"},
+    {"id" : 45, "date":"2018-10-18T19:00:00.000Z", "home":"Anderlecht", "away":"Paris", "stage":"Group B"},
+    {"id" : 46, "date":"2018-10-18T19:00:00.000Z", "home":"Chelsea", "away":"Roma", "stage":"Group C"},
+    {"id" : 47, "date":"2018-10-18T19:00:00.000Z", "home":"Barcelona", "away":"Olympiacos", "stage":"Group D"},
+    {"id" : 48, "date":"2018-10-18T19:00:00.000Z", "home":"Juventus", "away":"Sporting CP", "stage":"Group D"},
+    {"id" : 49, "date":"2018-10-31T19:00:00.000Z", "home":"Man. United", "away":"Benfica", "stage":"Group A"},
+    {"id" : 50, "date":"2018-10-31T19:00:00.000Z", "home":"Basel", "away":"CSKA Moskva", "stage":"Group A"},
+    {"id" : 51, "date":"2018-10-31T19:00:00.000Z", "home":"Celtic", "away":"Bayern", "stage":"Group B"},
+    {"id" : 52, "date":"2018-10-31T19:00:00.000Z", "home":"Paris", "away":"Anderlecht", "stage":"Group B"},
+    {"id" : 53, "date":"2018-10-31T19:00:00.000Z", "home":"Roma", "away":"Chelsea", "stage":"Group C"},
+    {"id" : 54, "date":"2018-10-31T19:00:00.000Z", "home":"Atlético", "away":"Qarabağ", "stage":"Group C"},
+    {"id" : 55, "date":"2018-10-31T19:00:00.000Z", "home":"Olympiacos", "away":"Barcelona", "stage":"Group D"},
+    {"id" : 56, "date":"2018-10-31T19:00:00.000Z", "home":"Sporting CP", "away":"Juventus", "stage":"Group D"},
+    {"id" : 57, "date":"2018-11-01T19:00:00.000Z", "home":"Beşiktaş", "away":"Monaco", "stage":"Group G"},
+    {"id" : 58, "date":"2018-11-01T19:00:00.000Z", "home":"Liverpool", "away":"Maribor", "stage":"Group E"},
+    {"id" : 59, "date":"2018-11-01T19:00:00.000Z", "home":"Sevilla", "away":"Spartak Moskva", "stage":"Group E"},
+    {"id" : 60, "date":"2018-11-01T19:00:00.000Z", "home":"Shakhtar Donetsk", "away":"Feyenoord", "stage":"Group F"},
+    {"id" : 61, "date":"2018-11-01T19:00:00.000Z", "home":"Napoli", "away":"Man. City", "stage":"Group F"},
+    {"id" : 62, "date":"2018-11-01T19:00:00.000Z", "home":"Porto", "away":"Leipzig", "stage":"Group G"},
+    {"id" : 63, "date":"2018-11-01T19:00:00.000Z", "home":"Tottenham", "away":"Real Madrid", "stage":"Group H"},
+    {"id" : 64, "date":"2018-11-01T19:00:00.000Z", "home":"Dortmund", "away":"APOEL", "stage":"Group H"},
+    {"id" : 65, "date":"2018-11-21T19:00:00.000Z", "home":"Spartak Moskva", "away":"Maribor", "stage":"Group E"},
+    {"id" : 66, "date":"2018-11-21T19:00:00.000Z", "home":"Beşiktaş", "away":"Porto", "stage":"Group G"},
+    {"id" : 67, "date":"2018-11-21T19:00:00.000Z", "home":"Sevilla", "away":"Liverpool", "stage":"Group E"},
+    {"id" : 68, "date":"2018-11-21T19:00:00.000Z", "home":"Napoli", "away":"Shakhtar Donetsk", "stage":"Group F"},
+    {"id" : 69, "date":"2018-11-21T19:00:00.000Z", "home":"Man. City", "away":"Feyenoord", "stage":"Group F"},
+    {"id" : 70, "date":"2018-11-21T19:00:00.000Z", "home":"Monaco", "away":"Leipzig", "stage":"Group G"},
+    {"id" : 71, "date":"2018-11-21T19:00:00.000Z", "home":"Dortmund", "away":"Tottenham", "stage":"Group H"},
+    {"id" : 72, "date":"2018-11-21T19:00:00.000Z", "home":"APOEL", "away":"Real Madrid", "stage":"Group H"},
+    {"id" : 73, "date":"2018-11-22T19:00:00.000Z", "home":"CSKA Moskva", "away":"Benfica", "stage":"Group A"},
+    {"id" : 74, "date":"2018-11-22T19:00:00.000Z", "home":"Qarabağ", "away":"Chelsea", "stage":"Group C"},
+    {"id" : 75, "date":"2018-11-22T19:00:00.000Z", "home":"Basel", "away":"Man. United", "stage":"Group A"},
+    {"id" : 76, "date":"2018-11-22T19:00:00.000Z", "home":"Paris", "away":"Celtic", "stage":"Group B"},
+    {"id" : 77, "date":"2018-11-22T19:00:00.000Z", "home":"Anderlecht", "away":"Bayern", "stage":"Group B"},
+    {"id" : 78, "date":"2018-11-22T19:00:00.000Z", "home":"Atlético", "away":"Roma", "stage":"Group C"},
+    {"id" : 79, "date":"2018-11-22T19:00:00.000Z", "home":"Sporting CP", "away":"Olympiacos", "stage":"Group D"},
+    {"id" : 80, "date":"2018-11-22T19:00:00.000Z", "home":"Juventus", "away":"Barcelona", "stage":"Group D"},
+    {"id" : 81, "date":"2018-12-05T19:00:00.000Z", "home":"Man. United", "away":"CSKA Moskva", "stage":"Group A"},
+    {"id" : 82, "date":"2018-12-05T19:00:00.000Z", "home":"Benfica", "away":"Basel", "stage":"Group A"},
+    {"id" : 83, "date":"2018-12-05T19:00:00.000Z", "home":"Celtic", "away":"Anderlecht", "stage":"Group B"},
+    {"id" : 84, "date":"2018-12-05T19:00:00.000Z", "home":"Bayern", "away":"Paris", "stage":"Group B"},
+    {"id" : 85, "date":"2018-12-05T19:00:00.000Z", "home":"Roma", "away":"Qarabağ", "stage":"Group C"},
+    {"id" : 86, "date":"2018-12-05T19:00:00.000Z", "home":"Chelsea", "away":"Atlético", "stage":"Group C"},
+    {"id" : 87, "date":"2018-12-05T19:00:00.000Z", "home":"Olympiacos", "away":"Juventus", "stage":"Group D"},
+    {"id" : 88, "date":"2018-12-05T19:00:00.000Z", "home":"Barcelona", "away":"Sporting CP", "stage":"Group D"},
+    {"id" : 89, "date":"2018-12-06T19:00:00.000Z", "home":"Liverpool", "away":"Spartak Moskva", "stage":"Group E"},
+    {"id" : 90, "date":"2018-12-06T19:00:00.000Z", "home":"Maribor", "away":"Sevilla", "stage":"Group E"},
+    {"id" : 91, "date":"2018-12-06T19:00:00.000Z", "home":"Shakhtar Donetsk", "away":"Man. City", "stage":"Group F"},
+    {"id" : 92, "date":"2018-12-06T19:00:00.000Z", "home":"Feyenoord", "away":"Napoli", "stage":"Group F"},
+    {"id" : 93, "date":"2018-12-06T19:00:00.000Z", "home":"Porto", "away":"Monaco", "stage":"Group G"},
+    {"id" : 94, "date":"2018-12-06T19:00:00.000Z", "home":"Leipzig", "away":"Beşiktaş", "stage":"Group G"},
+    {"id" : 95, "date":"2018-12-06T19:00:00.000Z", "home":"Tottenham", "away":"APOEL", "stage":"Group H"},
+    {"id" : 96, "date":"2018-12-06T19:00:00.000Z", "home":"Real Madrid", "away":"Dortmund", "stage":"Group H"},
 
-    {"id": 2,"date":"2018-06-15T12:00:00.000Z","home":"Egypt","away":"Uruguay","stage":"group stage"},
-    {"id": 3,"date":"2018-06-15T15:00:00.000Z","home":"Morocco","away":"Iran","stage":"group stage"},
-    {"id": 4,"date":"2018-06-15T18:00:00.000Z","home":"Portugal","away":"Spain","stage":"group stage"},
-
-    {"id": 5,"date":"2018-06-16T10:00:00.000Z","home":"France","away":"Australia","stage":"group stage"},
-    {"id": 6,"date":"2018-06-16T13:00:00.000Z","home":"Argentina","away":"Iceland","stage":"group stage"},
-    {"id": 7,"date":"2018-06-16T16:00:00.000Z","home":"Peru","away":"Denmark","stage":"group stage"},
-    {"id": 8,"date":"2018-06-16T19:00:00.000Z","home":"Croatia","away":"Nigeria","stage":"group stage"},
-
-    {"id": 9,"date":"2018-06-17T12:00:00.000Z","home":"Costa Rica","away":"Serbia","stage":"group stage"},
-    {"id":10,"date":"2018-06-17T15:00:00.000Z","home":"Germany","away":"Mexico","stage":"group stage"},
-    {"id":11,"date":"2018-06-17T18:00:00.000Z","home":"Brazil","away":"Switzerland","stage":"group stage"},
-
-    {"id":12,"date":"2018-06-18T12:00:00.000Z","home":"Sweden","away":"Korea Republic","stage":"group stage"},
-    {"id":13,"date":"2018-06-18T15:00:00.000Z","home":"Belgium","away":"Panama","stage":"group stage"},
-    {"id":14,"date":"2018-06-18T18:00:00.000Z","home":"Tunisia","away":"England","stage":"group stage"},
-
-    {"id":15,"date":"2018-06-19T12:00:00.000Z","home":"Colombia","away":"Japan","stage":"group stage"},
-    {"id":16,"date":"2018-06-19T15:00:00.000Z","home":"Poland","away":"Senegal","stage":"group stage"},
-    {"id":17,"date":"2018-06-19T18:00:00.000Z","home":"Russia","away":"Egypt","stage":"group stage"},
-
-    {"id":18,"date":"2018-06-20T12:00:00.000Z","home":"Portugal","away":"Morocco","stage":"group stage"},
-    {"id":19,"date":"2018-06-20T15:00:00.000Z","home":"Uruguay","away":"Saudi Arabia","stage":"group stage"},
-    {"id":20,"date":"2018-06-20T18:00:00.000Z","home":"Iran","away":"Spain","stage":"group stage"},
-
-    {"id":21,"date":"2018-06-21T12:00:00.000Z","home":"Denmark","away":"Australia","stage":"group stage"},
-    {"id":22,"date":"2018-06-21T15:00:00.000Z","home":"France","away":"Peru","stage":"group stage"},
-    {"id":23,"date":"2018-06-21T18:00:00.000Z","home":"Argentina","away":"Croatia","stage":"group stage"},
-
-    {"id":24,"date":"2018-06-22T12:00:00.000Z","home":"Brazil","away":"Costa Rica","stage":"group stage"},
-    {"id":25,"date":"2018-06-22T15:00:00.000Z","home":"Nigeria","away":"Iceland","stage":"group stage"},
-    {"id":26,"date":"2018-06-22T18:00:00.000Z","home":"Serbia","away":"Switzerland","stage":"group stage"},
-
-    {"id":27,"date":"2018-06-23T12:00:00.000Z","home":"Belgium","away":"Tunisia","stage":"group stage"},
-    {"id":28,"date":"2018-06-23T18:00:00.000Z","home":"Germany","away":"Sweden","stage":"group stage"},
-    {"id":29,"date":"2018-06-23T15:00:00.000Z","home":"Korea Republic","away":"Mexico","stage":"group stage"},
-
-    {"id":30,"date":"2018-06-24T12:00:00.000Z","home":"England","away":"Panama","stage":"group stage"},
-    {"id":31,"date":"2018-06-24T15:00:00.000Z","home":"Japan","away":"Senegal","stage":"group stage"},
-    {"id":32,"date":"2018-06-24T18:00:00.000Z","home":"Poland","away":"Colombia","stage":"group stage"},
-
-    {"id":33,"date":"2018-06-25T14:00:00.000Z","home":"Uruguay","away":"Russia","stage":"group stage"},
-    {"id":34,"date":"2018-06-25T14:00:00.000Z","home":"Saudi Arabia","away":"Egypt","stage":"group stage"},
-    {"id":35,"date":"2018-06-25T18:00:00.000Z","home":"Iran","away":"Portugal","stage":"group stage"},
-    {"id":36,"date":"2018-06-25T18:00:00.000Z","home":"Spain","away":"Morocco","stage":"group stage"},
-
-    {"id":37,"date":"2018-06-26T14:00:00.000Z","home":"Denmark","away":"France","stage":"group stage"},
-    {"id":38,"date":"2018-06-26T14:00:00.000Z","home":"Australia","away":"Peru","stage":"group stage"},
-    {"id":39,"date":"2018-06-26T18:00:00.000Z","home":"Nigeria","away":"Argentina","stage":"group stage"},
-    {"id":40,"date":"2018-06-26T18:00:00.000Z","home":"Iceland","away":"Croatia","stage":"group stage"},
-
-    {"id":41,"date":"2018-06-27T14:00:00.000Z","home":"Korea Republic","away":"Germany","stage":"group stage"},
-    {"id":42,"date":"2018-06-27T14:00:00.000Z","home":"Mexico","away":"Sweden","stage":"group stage"},
-    {"id":43,"date":"2018-06-27T18:00:00.000Z","home":"Serbia","away":"Brazil","stage":"group stage"},
-    {"id":44,"date":"2018-06-27T18:00:00.000Z","home":"Switzerland","away":"Costa Rica","stage":"group stage"},
-
-    {"id":45,"date":"2018-06-28T14:00:00.000Z","home":"Japan","away":"Poland","stage":"group stage"},
-    {"id":46,"date":"2018-06-28T14:00:00.000Z","home":"Senegal","away":"Colombia","stage":"group stage"},
-    {"id":47,"date":"2018-06-28T18:00:00.000Z","home":"England","away":"Belgium","stage":"group stage"},
-    {"id":48,"date":"2018-06-28T18:00:00.000Z","home":"Panama","away":"Tunisia","stage":"group stage"},
-
-    {"id":49,"date":"2018-06-30T14:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-    {"id":50,"date":"2018-06-30T18:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-
-    {"id":51,"date":"2018-07-01T14:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-    {"id":52,"date":"2018-07-01T18:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-
-    {"id":53,"date":"2018-07-02T14:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-    {"id":54,"date":"2018-07-02T18:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-
-    {"id":55,"date":"2018-07-03T14:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-    {"id":56,"date":"2018-07-03T18:00:00.000Z","home":"TBD","away":"TBD","stage":"round of 16"},
-
-    {"id":57,"date":"2018-07-06T14:00:00.000Z","home":"TBD","away":"TBD","stage":"quarter finals"},
-    {"id":58,"date":"2018-07-06T18:00:00.000Z","home":"TBD","away":"TBD","stage":"quarter finals"},
-
-    {"id":59,"date":"2018-07-07T14:00:00.000Z","home":"TBD","away":"TBD","stage":"quarter finals"},
-    {"id":60,"date":"2018-07-07T18:00:00.000Z","home":"TBD","away":"TBD","stage":"quarter finals"},
-
-    {"id":61,"date":"2018-07-10T18:00:00.000Z","home":"TBD","away":"TBD","stage":"semi finals"},
-
-    {"id":62,"date":"2018-07-11T18:00:00.000Z","home":"TBD","away":"TBD","stage":"semi finals"},
-
-    {"id":63,"date":"2018-07-14T14:00:00.000Z","home":"TBD","away":"TBD","stage":"third place"},
-
-    {"id":64,"date":"2018-07-15T15:00:00.000Z","home":"TBD","away":"TBD","stage":"final"}]);
-
-  done();
+  ])
+    .then(() => done());
 };
 
 module.exports.down = function (done) {
-  this.db.collection('match').deleteMany({});
-
   done();
 };
