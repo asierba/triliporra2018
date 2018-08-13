210c916865f2a0868a3fe7a33d2182e201571584
Asier <1537266+asierba@users.noreply.github.com>
2018-08-13 17:01:27 +0100

    Add flags for teams

--- src/web/components/match/teamFlag.tsx
+++ src/web/components/match/teamFlag.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 
+const smallStyle = {
+  width : '30px'
+};
+const regularStyle = {
+  height: '60px'
+};
+
 export default function TeamFlag(props) {
   const teamName = props.name.replace(' ','-');
   const src = `images/flags/${teamName}.png`;
   const alt = `flag ${teamName}`;
 
-  const smallStyle = {
-    width : '30px'
-  };
-  const style = props.small ? smallStyle : {};
+  const style = props.small ? smallStyle : regularStyle;
   return <img alt={alt} src={src} style={style} />;
 }
